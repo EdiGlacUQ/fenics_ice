0a36f145f36a92d01ed91d4152d8f5f0aa8eade4
cpk26 <cpk26@cam.ac.uk>
2018-07-10 11:07:08 +0100

    Minor fixes

--- runs/run_balancemeltrates.py
+++ runs/run_balancemeltrates.py
@@ -36,12 +36,13 @@ def main(dd, outdir, run_length, n_steps, init_yr):
     M = FunctionSpace(mesh, 'DG', 0)
     Q = FunctionSpace(mesh, 'Lagrange', 1) if os.path.isfile(os.path.join(dd,'param.p')) else M
 
+    mask = Function(M,os.path.join(dd,'mask.xml'))
+
     if os.path.isfile(os.path.join(dd,'data_mesh.xml')):
         data_mesh = Mesh(os.path.join(dd,'data_mesh.xml'))
         Mdata = FunctionSpace(data_mesh, 'DG', 0)
         data_mask = Function(Mdata, os.path.join(dd,'data_mask.xml'))
     else:
-        mask = Function(M,os.path.join(dd,'mask.xml'))
         data_mesh = mesh
         data_mask = mask
 
--- runs/run_inv.py
+++ runs/run_inv.py
@@ -26,12 +26,13 @@ def main(maxiter, rc_inv, pflag, outdir, dd, nx, ny, sim_flag, bflag, altiter):
     M = FunctionSpace(mesh, 'DG', 0)
     Q = FunctionSpace(mesh, 'Lagrange', 1) if os.path.isfile(os.path.join(dd,'param.p')) else M
 
+    mask = Function(M,os.path.join(dd,'mask.xml'))
+
     if os.path.isfile(os.path.join(dd,'data_mesh.xml')):
         data_mesh = Mesh(os.path.join(dd,'data_mesh.xml'))
         Mdata = FunctionSpace(data_mesh, 'DG', 0)
         data_mask = Function(Mdata, os.path.join(dd,'data_mask.xml'))
     else:
-        mask = Function(M,os.path.join(dd,'mask.xml'))
         data_mesh = mesh
         data_mask = mask
 
