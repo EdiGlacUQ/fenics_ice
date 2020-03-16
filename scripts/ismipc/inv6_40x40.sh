53b8f12aa333754b0d3533be1f2a830637a03327
cpk26 <ckoziol@gmail.com>
2020-03-16 15:35:04 -0700

    Added ismipc scripts to git

---
+++ scripts/ismipc/inv6_40x40.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+BASE_DIR=$FENICS_ICE_BASE_DIR
+RUN_DIR=$BASE_DIR/runs
+
+INPUT_DIR=$BASE_DIR/input/ismipC
+OUTPUT_DIR=$BASE_DIR/output/ismipC/ismipC_inv6_perbc_40x40_gnhep_prior
+EIGENDECOMP_DIR=$OUTPUT_DIR/run_forward
+FORWARD_DIR=$OUTPUT_DIR/run_forward
+
+EIGFILE=slepceig_all.p
+
+RC1=1.0
+RC2=1e-2
+RC3=1e-2
+RC4=1e6
+RC5=1e6
+
+
+T=15.0
+N=60
+S=5
+
+NX=40
+NY=40
+
+QOI=1
+
+cd $RUN_DIR
+
+python run_inv.py -b -x $NX -y $NY -m 200 -p 0  -r $RC1 $RC2 $RC3 $RC4 $RC5 -d $INPUT_DIR -o $OUTPUT_DIR
+python run_forward.py -t $T -n $N -s $S -i $QOI -d $OUTPUT_DIR -o $FORWARD_DIR
+python run_eigendec.py -s -m -p 0  -d $OUTPUT_DIR -o $EIGENDECOMP_DIR -f $EIGFILE
+python run_errorprop.py -p 0 -d $FORWARD_DIR -e $EIGENDECOMP_DIR -l $EIGFILE -o $FORWARD_DIR
---
+++ scripts/ismipc/run_all.sh
@@ -0,0 +1,4 @@
+./inv4.sh 2>&1 | tee inv4.log;
+./inv6.sh 2>&1 | tee inv6.log;
+./inv6_30x30.sh 2>&1 | tee inv6_30x30.log;
+./inv6_40x40.sh 2>&1 | tee inv6_40x40.log
