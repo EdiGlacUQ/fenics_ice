641f0796f4f6625a5f19a113de11a513f29023db
J Todd <jat39@st-andrews.ac.uk>
2020-05-07 16:16:54 +0100

    Switch run_momsolve.py over to toml config

--- runs/run_momsolve.py
+++ runs/run_momsolve.py
@@ -1,3 +1,8 @@
+"""
+Run the momentum solver only. Primarily used to generate velocity field
+for ismip-c case before running the main model.
+"""
+
 import sys
 sys.path.insert(0,'../code/')
 import os
@@ -5,6 +10,8 @@ import argparse
 from fenics import *
 import model
 import solver
+import mesh as fice_mesh
+from config import *
 import numpy as np
 import time
 import datetime
@@ -12,154 +19,37 @@ import pickle
 from IPython import embed
 
 
-def main(outdir, dd, periodic_bc, nx, ny, sl):
+def main(config_file):
+
+    #Read run config file
+    params = ConfigParser(config_file)
+
+    dd = params.io.input_dir
 
     # Determine Mesh
+    mesh = fice_mesh.get_mesh(params)
+    data_mesh = fice_mesh.get_data_mesh(params)
 
-    # Create a new mesh with specific resolution
-    if nx and ny:
-        data_mesh_file = 'data_mesh.xml'
-        data_mask_file = 'data_mask.xml'
-
-        assert(os.path.isfile(os.path.join(dd,data_mesh_file))), 'Need data_mesh.xml to interpolate'
-        assert(os.path.isfile(os.path.join(dd,data_mask_file))), 'Need data_mask.xml to interpolate'
-
-        #Generate model mesh
-        print('Generating new mesh')
-        gf = 'grid_data.npz'
-        npzfile = np.load(os.path.join(dd,'grid_data.npz'))
-        xlim = npzfile['xlim']
-        ylim = npzfile['ylim']
-
-        mesh = RectangleMesh(Point(xlim[0],ylim[0]), Point(xlim[-1], ylim[-1]), nx, ny)
-
-    # Reuse a mesh; in this case, mesh and data_mesh will be identical
-
-    # Otherwise see if there is previous run
-    elif os.path.isfile(os.path.join(dd,'mesh.xml')):
-        data_mesh_file = 'mesh.xml'
-        data_mask_file = 'mask.xml'
-
-        mesh = Mesh(os.path.join(dd,data_mesh_file))
-    
-    # Mirror data files
-    elif os.path.isfile(os.path.join(dd,'data_mesh.xml')):
-        #Start from raw data
-        data_mesh_file = 'data_mesh.xml'
-        data_mask_file = 'data_mask.xml'
-
-        mesh = Mesh(os.path.join(dd,data_mesh_file))
-
-    else:
-        print('Need mesh and mask files')
-        raise SystemExit
-
-    data_mesh = Mesh(os.path.join(dd,data_mesh_file))
-    
     # Define Function Spaces
     M = FunctionSpace(data_mesh, 'DG', 0)
     Q = FunctionSpace(data_mesh, 'Lagrange', 1)
-    Qp = Q
 
     # Make necessary modification for periodic bc
-    if periodic_bc:
-        mesh_length = np.NaN
-
-        # If we're on a new mesh
-        if nx and ny:
-            L1 = xlim[-1] - xlim[0]
-            L2 = ylim[-1] - ylim[0]
-            assert( L1==L2), 'Periodic Boundary Conditions require a square domain'
-            mesh_length = L1
-
-        # If previous run   
-        elif os.path.isfile(os.path.join(dd,'param.p')):
-            mesh_length = pickle.load(open(os.path.join(dd,'param.p'), 'rb'))['periodic_bc']
-            assert(mesh_length), 'Need to run periodic bc using original files'
-
-        # Assume we're on a data_mesh
-        else:
-            gf = 'grid_data.npz'
-            npzfile = np.load(os.path.join(dd,'grid_data.npz'))
-            xlim = npzfile['xlim']
-            ylim = npzfile['ylim']
-            L1 = xlim[-1] - xlim[0]
-            L2 = ylim[-1] - ylim[0]
-            assert( L1==L2), 'Periodic Boundary Conditions require a square domain'
-            mesh_length = L1
-
-        Qp = FunctionSpace(data_mesh,'Lagrange',1,constrained_domain=model.PeriodicBoundary(mesh_length))
-    
+    if params.mesh.periodic_bc:
+        Qp = fice_mesh.setup_periodic_bc(params, data_mesh)
+    else:
+        Qp = Q
 
 
-    data_mask = Function(M,os.path.join(dd,data_mask_file))
+    data_mask = fice_mesh.get_data_mask(params, M)
 
-    bed = Function(Q,os.path.join(dd,'bed.xml'))
-    bmelt = Function(M,os.path.join(dd,'bmelt.xml'))
-    smb = Function(M,os.path.join(dd,'smb.xml'))
-    thick = Function(M,os.path.join(dd,'thick.xml'))
-    alpha = Function(Qp,os.path.join(dd,'alpha.xml'))
+    bed = Function(Q, os.path.join(dd, 'bed.xml'))
+    bmelt = Function(M, os.path.join(dd, 'bmelt.xml'))
+    smb = Function(M, os.path.join(dd, 'smb.xml'))
+    thick = Function(M, os.path.join(dd, 'thick.xml'))
+    alpha = Function(Qp, os.path.join(dd, 'alpha.xml'))
 
-#############################
-    # #Load Data
-    # data_mesh = Mesh(os.path.join(dd,'mesh.xml'))
-    # mesh = data_mesh
-
-    # M = FunctionSpace(data_mesh, 'DG', 0)
-    # Mp = M
-    # Q = FunctionSpace(data_mesh, 'Lagrange', 1) if os.path.isfile(os.path.join(dd,'param.p')) else M
-    # Qp = Q
-
-    # bed = Function(Q,os.path.join(dd,'bed.xml'))
-    # bmelt = Function(M,os.path.join(dd,'bmelt.xml'))
-    # smb = Function(M,os.path.join(dd,'smb.xml'))
-    # thick = Function(M,os.path.join(dd,'thick.xml'))
-    # mask = Function(M,os.path.join(dd,'mask.xml'))
-    # alpha = Function(Qp,os.path.join(dd,'alpha.xml'))
-
-
-    # if not os.path.isfile(os.path.join(dd,'param.p')):
-    #     print('Generating new mesh')
-    #     #Generate model mesh
-    #     gf = 'grid_data.npz'
-    #     npzfile = np.load(os.path.join(dd,'grid_data.npz'))
-    #     xlim = npzfile['xlim']
-    #     ylim = npzfile['ylim']
-
-    #     if not nx:
-    #         nx = int(npzfile['nx'])
-    #     if not ny:
-    #         ny = int(npzfile['ny'])
-
-    #     mesh = RectangleMesh(Point(xlim[0],ylim[0]), Point(xlim[-1], ylim[-1]), nx, ny)
-    # else:
-    #     print('Identified as previous run, reusing mesh')
-    #     assert(not nx), 'Cannot change mesh resolution from previous run'
-    #     assert(not ny), 'Cannot change mesh resolution from previous run'
-
-    # if periodic_bc:
-    #     if os.path.isfile(os.path.join(dd,'param.p')):
-    #         periodic_bc = pickle.load(open(os.path.join(dd,'param.p'), 'rb'))['periodic_bc']
-    #         assert(periodic_bc), 'Need to run periodic bc using original files'
-    #     else:
-    #         L1 = xlim[-1] - xlim[0]
-    #         L2 = ylim[-1] - ylim[0]
-    #         assert( L1==L2), 'Periodic Boundary Conditions require a square domain'
-    #         periodic_bc = L1
-
-    #     Qp = FunctionSpace(mesh,'Lagrange',1,constrained_domain=model.PeriodicBoundary(periodic_bc))
-############################
-
-
-    #Initialize Model
-    param = {
-            'outdir' : outdir,
-            'periodic_bc': mesh_length,
-            'sliding_law': sl
-            }
-
-
-    mdl = model.model(mesh,data_mask, param)
+    mdl = model.model(mesh, data_mask, params)
     mdl.init_bed(bed)
     mdl.init_thick(thick)
     mdl.gen_surf()
@@ -185,10 +75,8 @@ def main(outdir, dd, periodic_bc, nx, ny, sl):
     slvr.solve_mom_eq()
 
 
-
-
     #Output model variables in ParaView+Fenics friendly format
-    outdir = mdl.param['outdir']
+    outdir = params.io.output_dir
     pickle.dump( mdl.param, open( os.path.join(outdir,'param.p'), "wb" ) )
 
     File(os.path.join(outdir,'mesh.xml')) << mdl.mesh
@@ -247,34 +135,5 @@ def main(outdir, dd, periodic_bc, nx, ny, sl):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--boundaries', dest='periodic_bc', action='store_true', help='Periodic boundary conditions')
-    parser.add_argument('-x', '--cells_x', dest='nx', type=int, help='Number of cells in x direction')
-    parser.add_argument('-y', '--cells_y', dest='ny', type=int, help='Number of cells in y direction')
-    parser.add_argument('-q', '--slidinglaw', dest='sl', type=int,  help = 'Sliding Law (0: linear (default), 1: weertman)')
-    
-    parser.add_argument('-o', '--outdir', dest='outdir', type=str, help='Directory to store output')
-    parser.add_argument('-d', '--datadir', dest='dd', type=str, required=True, help='Directory with input data')
-   
-
-    parser.set_defaults(periodic_bc=False,nx=False,ny=False, sl=0)
-    args = parser.parse_args()
-
-    outdir = args.outdir
-    dd = args.dd
-    periodic_bc = args.periodic_bc
-    nx = args.nx
-    ny = args.ny
-    sl = args.sl
-
-    if not outdir:
-        outdir = ''.join(['./run_momsolve_', datetime.datetime.now().strftime("%m%d%H%M%S")])
-        print('Creating output directory: {0}'.format(outdir))
-        os.makedirs(outdir)
-    else:
-        if not os.path.exists(outdir):
-            os.makedirs(outdir)
-
-
-
-    main(outdir, dd, periodic_bc, nx, ny, sl)
+    assert len(sys.argv) == 2, "Expected a configuration file (*.toml)"
+    main(sys.argv[1])
