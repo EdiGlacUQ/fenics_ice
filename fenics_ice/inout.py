ebcaf23d61380d62f00bd9c6c0dbadf49f5c5193
J Todd <jat39@st-andrews.ac.uk>
2020-06-10 10:41:28 +0100

    New functions for reading, writing & interpolating fields

--- fenics_ice/inout.py
+++ fenics_ice/inout.py
@@ -5,9 +5,17 @@ Module to handle model input & output
 import sys
 from pathlib import Path
 import pickle
-import numpy as np
-from fenics import *
 import logging
+import re
+import h5py
+from scipy import interpolate as interp
+from IPython import embed
+
+from fenics import *
+import numpy as np
+
+# Regex for catching unnamed vars
+unnamed_re = re.compile("f_[0-9]+")
 
 def write_qval(Qval, params):
     """
@@ -49,6 +57,157 @@ def write_dqval(dQ_ts, params):
 
     hdf5out.close()
 
+def read_variable(name, space, params, indir=False, filename=None):
+    """
+    Read variable from file, assumed prefixed with run_name
+    unless filename is given.
+    By default, looks in output dir (unless indir=True)
+    """
+
+    if indir:
+        tgt_dir = params.io.input_dir
+    else:
+        tgt_dir = params.io.output_dir
+
+    if not filename:
+        infname = (Path(tgt_dir)/ \
+                  "_".join((params.io.run_name, name))).with_suffix(".xml")
+        filename = str(infname)
+
+    var = Function(space, filename)
+    return var
+
+def write_variable(var, params, name=None):
+    """
+    Produce xml & vtk output of supplied variable (prefixed with run name)
+    Name is taken from variable structure if not provided
+    """
+
+    #Take variable's inbuilt name by default
+    if not name:
+        name = var.name()
+        if unnamed_re.match(name):
+            logging.error("Attempted to write out an unnamed variable %s" % name)
+            raise Exception
+
+    #Prefix the run name
+    outfname = Path(params.io.output_dir)/"_".join((params.io.run_name,name))
+    vtk_fname = str(outfname.with_suffix(".pvd"))
+    xml_fname = str(outfname.with_suffix(".xml"))
+
+    File(vtk_fname) << var
+    File(xml_fname) << var
+
+    logging.info("Writing function %s to file %s" % (name, outfname))
+
+def read_vel_obs(params, model=None):
+    """
+    Read velocity observations & uncertainty from HDF5 file
+
+    For now, expects an HDF5 file
+    """
+
+    infile = Path(params.io.input_dir) / params.obs.vel_file
+    assert infile.exists(), f"Couldn't find velocity observations file: {infile}"
+
+    infile = h5py.File(infile, 'r')
+
+    x_obs = infile['x'][:, 0]
+    y_obs = infile['y'][:, 0]
+    u_obs = infile['u_obs'][:, 0]
+    v_obs = infile['v_obs'][:, 0]
+    u_std = infile['u_std'][:, 0]
+    v_std = infile['v_std'][:, 0]
+    mask_vel = infile['mask_vel'][:, 0]
+
+    uv_obs_pts = np.vstack((x_obs, y_obs)).T
+
+    if model is not None:
+        model.uv_obs_pts = uv_obs_pts
+        model.u_obs = u_obs
+        model.v_obs = v_obs
+        model.u_std = u_std
+        model.v_std = v_std
+        model.mask_vel = mask_vel
+    else:
+        return uv_obs_pts, u_obs, v_obs, u_std, v_std, mask_vel
+
+class InputData(object):
+    """Loads gridded data & defines interpolators"""
+
+    def __init__(self, infile):
+        self.infile = Path(infile)
+
+        # Check the input file is a HDF5
+        assert self.infile.exists(), "No input file found"
+        assert self.infile.suffix == '.h5'
+
+        self.read_data()
+
+    def read_data(self):
+        """
+        Load metadata attributes & fields (as numpy arrays) from infile
+
+        Note that the fields to find are not prescribed - all are read
+        TODO - is this a good idea?
+        """
+        indata = h5py.File(self.infile, 'r')
+        self.nx   = indata.attrs['nx']
+        self.ny   = indata.attrs['ny']
+        self.xmin = indata.attrs['xmin']
+        self.xmax = indata.attrs['xmax']
+        self.ymin = indata.attrs['ymin']
+        self.ymax = indata.attrs['ymax']
+
+        for name, data in indata.items():
+            setattr(self, name, data[:])
+
+        # self.thick = indata['thick'][:]
+        # self.surf = indata['surf'][:]
+        # self.bed = indata['bed'][:]
+        # self.alpha = indata['alpha'][:]
+        # self.bmelt = indata['bmelt'][:]
+        # self.smb = indata['smb'][:]
+        # self.B2 = indata['B2'][:]
+
+    def interpolate(self, name, space, default=None):
+        """
+        Interpolate named variable onto function space
+
+        Arguments:
+        name : the variable to be interpolated (need not necessarily exist!)
+        space : function space onto which to interpolate
+        default : value to return if field is absent (otherwise raise error)
+
+        Returns:
+        function : the interpolated function
+        """
+
+        function = Function(space, name=name)
+
+        try:
+            field = getattr(self, name)
+
+        except AttributeError:
+            # Fill with default, if supplied, else raise error
+            if default is not None:
+                logging.warn(f"No data found for {name},"
+                             f"filling with default value {default}")
+                function.vector()[:] = default
+                function.vector().apply("insert")
+                return function
+            else:
+                print(f"Failed to find field {name} in"
+                      f" input file {self.infile}")
+                raise
+
+        interper = interp.RegularGridInterpolator((self.x, self.y), field)
+        out_coords = space.tabulate_dof_coordinates()
+
+        result = interper(out_coords)
+        function.vector()[:] = result
+        function.vector().apply("insert")
+        return function
 
 # Custom formatter
 class LogFormatter(logging.Formatter):
