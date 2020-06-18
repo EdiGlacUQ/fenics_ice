2ac18348ed97b34066d4d79492ac0fb444d47e42
J Todd <jat39@st-andrews.ac.uk>
2020-06-18 20:59:39 +0100

    Refactor gen_rect_mesh for run-time call & MPI

--- aux/gen_rect_mesh.py
+++ aux/gen_rect_mesh.py
@@ -3,32 +3,41 @@ Generate a rectangular mesh for fenics sims. To replace the current case of
 run_inv.py making the mesh on the fly.
 """
 
+from mpi4py import MPI
 import argparse
 from fenics import RectangleMesh, Point, File
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-nx', '--nx', dest='nx', type=int,
-                    help='Number of cells along x-axis direction')
-parser.add_argument('-ny', '--ny', dest='ny', type=int,
-                    help='Number of cells along y-axis direction')
-parser.add_argument('-xmin', '--xmin', dest='xmin', type=float,
-                    help='Min x coordinate')
-parser.add_argument('-xmax', '--xmax', dest='xmax', type=float,
-                    help='Max x coordinate')
-parser.add_argument('-ymin', '--ymin', dest='ymin', type=float,
-                    help='Min y coordinate')
-parser.add_argument('-ymax', '--ymax', dest='ymax', type=float,
-                    help='Max y coordinate')
-parser.add_argument('-o', '--outfile', dest='outfile', type=str,
-                    help='Mesh output filename (eg.xml)')
-parser.add_argument('-d', '--direction', dest='direction', type=str,
-                    help="Left, Right or Crossed diagonals")
-parser.set_defaults(outfile="mesh.xml", xmin=0, ymin=0, direction='right')
+def main(nx, ny, xmin, xmax, ymin, ymax, outfile, direction='right'):
 
-args = parser.parse_args()
+    mesh = RectangleMesh(MPI.COMM_SELF,
+                         Point(xmin, ymin),
+                         Point(xmax, ymax),
+                         nx, ny, direction)
 
-mesh = RectangleMesh(Point(args.xmin, args.ymin),
-                     Point(args.xmax, args.ymax),
-                     args.nx, args.ny, args.direction)
+    File(MPI.COMM_SELF, outfile) << mesh
 
-File(args.outfile) << mesh
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-nx', '--nx', dest='nx', type=int,
+                        help='Number of cells along x-axis direction')
+    parser.add_argument('-ny', '--ny', dest='ny', type=int,
+                        help='Number of cells along y-axis direction')
+    parser.add_argument('-xmin', '--xmin', dest='xmin', type=float,
+                        help='Min x coordinate')
+    parser.add_argument('-xmax', '--xmax', dest='xmax', type=float,
+                        help='Max x coordinate')
+    parser.add_argument('-ymin', '--ymin', dest='ymin', type=float,
+                        help='Min y coordinate')
+    parser.add_argument('-ymax', '--ymax', dest='ymax', type=float,
+                        help='Max y coordinate')
+    parser.add_argument('-o', '--outfile', dest='outfile', type=str,
+                        help='Mesh output filename (eg.xml)')
+    parser.add_argument('-d', '--direction', dest='direction', type=str,
+                        help="Left, Right or Crossed diagonals")
+    parser.set_defaults(outfile="mesh.xml", xmin=0, ymin=0, direction='right')
+
+    args = parser.parse_args()
+
+    main(args.nx, args.ny, args.xmin,
+         args.xmax, args.ymin, args.ymax,
+         args.outfile, args.direction)
