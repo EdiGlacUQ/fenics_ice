a1477c73a5ac907cbe67ec9838b7596ebb656b1c
J Todd <jat39@st-andrews.ac.uk>
2021-02-02 13:07:51 +0000

    Reduce to single definition of prior

--- fenics_ice/prior.py
+++ fenics_ice/prior.py
@@ -18,6 +18,7 @@
 from dolfin import *
 from tlm_adjoint import *
 from .decorators import count_calls, timer
+import ufl
 
 class laplacian(object):
 
@@ -39,19 +40,24 @@ class laplacian(object):
         self.alpha_active = invparam.alpha_active
         self.beta_active = invparam.beta_active
 
+        self.test = TestFunctions(space)
+        self.trial = TrialFunctions(space)
+
+        # Mass and curvature terms
         if self.vdim == 1:
-            test, trial = TestFunction(space), TrialFunction(space)
+            test = self.test[0]
+            trial = self.trial[0]
             var_m = inner(test, trial) * dx
             var_n = inner(grad(test), grad(trial)) * dx
             self.M = assemble(var_m)
 
         else:
-            test_1, test_2 = TestFunctions(space)
-            trial_1, trial_2 = TrialFunctions(space)
-            var_m_1 = inner(test_1, trial_1) * dx
-            var_m_2 = inner(test_2, trial_2) * dx
-            var_n_1 = inner(grad(test_1), grad(trial_1)) * dx
-            var_n_2 = inner(grad(test_2), grad(trial_2)) * dx
+            test_0, test_1 = self.test
+            trial_0, trial_1 = self.trial
+            var_m[0] = inner(test_0, trial_0) * dx
+            var_m[1] = inner(test_1, trial_1) * dx
+            var_n[0] = inner(grad(test_0), grad(trial_0)) * dx
+            var_n[1] = inner(grad(test_1), grad(trial_1)) * dx
             self.M = assemble(var_m_1 + var_m_2)
 
 
@@ -60,17 +66,21 @@ class laplacian(object):
                                          "relative_tolerance": 1.0e-14})
         self.M_solver.set_operator(self.M)
 
+        #################################
+        # Definition of prior form here!
+        #################################
+        self.alpha_form = self.delta_alpha * var_m + self.gamma_alpha * var_n
+        self.beta_form = self.delta_beta * var_m + self.gamma_beta * var_n
+
         if self.vdim == 1:
             if invparam.alpha_active:
-                self.A = assemble(self.delta_alpha * var_m + self.gamma_alpha * var_n)
+                self.A_form = self.alpha_form
             else:
-                self.A = assemble(self.delta_beta * var_m + self.gamma_beta * var_n)
+                self.A_form = self.beta_form
         else:
-            self.A = assemble((self.delta_alpha * var_m_1) +
-                              (self.delta_beta * var_m_2) +
-                              (self.gamma_alpha * var_n_1) +
-                              (self.gamma_beta * var_n_2))
+            self.A_form = self.alpha_form + self.beta_form
 
+        self.A = assemble(self.A_form)
         self.A_solver = KrylovSolver("cg", "sor")
         self.A_solver.parameters.update({"absolute_tolerance": 1.0e-32,
                                          "relative_tolerance": 1.0e-14})
@@ -103,36 +113,46 @@ class laplacian(object):
         y.set_local(self.tmp1.get_local())
         y.apply("insert")
 
+    def my_norm(self, fun):
+        """Return the 0.5 inner product of the reg term"""
+        return 0.5 * inner(fun, fun)*dx
+
     def J_reg(self, alpha, beta):
-        """Compute the regularisation term of the cost function"""
+        """
+        Compute the regularisation term of the cost function
+
+        Returns a list of 1 or 2 terms depending on inversion type.
+        """
 
         assert alpha is not None or beta is not None
-        space = function_space(alpha) if alpha is not None else function_space(beta)
+        space = self.space
 
         result = [None, None]
 
-        trial = TrialFunction(space)
-        test = TestFunction(space)
-
         if self.alpha_active:
-            a = trial * test * dx
-            f_alpha = Function(space, name='f_alpha')
-            L = (self.delta_alpha * alpha * test
-                 + self.gamma_alpha*inner(grad(alpha), grad(test)))*dx
-            solve(a == L, f_alpha)
-            J_reg_alpha = 0.5 * inner(f_alpha, f_alpha)*dx
 
+            test = self.test[0]
+            trial = self.trial[0]
+
+            f_alpha = Function(space, name='f_alpha')
+            L = ufl.replace(self.alpha_form, {trial: alpha})
+            a = test * trial * dx
+
+            solve(a == L, f_alpha)
+            J_reg_alpha = self.my_norm(f_alpha)
             result[0] = J_reg_alpha
 
         if self.beta_active:
 
-            a = trial * test * dx
-            f_beta = Function(space, name='f_beta')
-            L = (self.delta_beta * beta * test
-                 + self.gamma_beta*inner(grad(beta), grad(test)))*dx
-            solve(a == L, f_beta)
-            J_reg_beta = 0.5 * inner(f_beta, f_beta)*dx
+            test = self.test[1]
+            trial = self.trial[1]
 
+            f_beta = Function(space, name='f_beta')
+            L = ufl.replace(self.beta_form, {trial: beta})
+            a = test * trial * dx
+
+            solve(a == L, f_beta)
+            J_reg_beta = self.my_norm(f_beta)
             result[1] = J_reg_beta
 
         return result
