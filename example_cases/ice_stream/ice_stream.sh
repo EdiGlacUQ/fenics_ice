78a295caced49208f59ba1cd2d27626a35781983
J Todd <jat39@st-andrews.ac.uk>
2021-02-08 10:29:32 +0000

    Minor fix - errorprop & invsigma can now run parallel

--- example_cases/ice_stream/ice_stream.sh
+++ example_cases/ice_stream/ice_stream.sh
@@ -11,5 +11,5 @@ RUN_DIR=$FENICS_ICE_BASE_DIR/runs/
 mpirun -n 3 python $RUN_DIR/run_inv.py ice_stream.toml
 mpirun -n 3 python $RUN_DIR/run_forward.py ice_stream.toml
 mpirun -n 3 python $RUN_DIR/run_eigendec.py ice_stream.toml
-python $RUN_DIR/run_errorprop.py ice_stream.toml
-python $RUN_DIR/run_invsigma.py ice_stream.toml
+mpirun -n 3 python $RUN_DIR/run_errorprop.py ice_stream.toml
+mpirun -n 3 python $RUN_DIR/run_invsigma.py ice_stream.toml
