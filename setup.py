a38bd357fff7bea27239fa732815810e5f966025
bearecinos <beatriz.recinos@ed.ac.uk>
2022-07-12 15:53:16 +0100

    checked with flake8

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 import setuptools
 if __name__ == "__main__":
     setuptools.setup(
-    name="fenics_ice",
-    description="A finite element model framework "
-                "that quantifies the initialization uncertainty for "
-                "time-dependent ice sheet models.",
-    url="https://github.com/EdiGlacUQ/fenics_ice.git",
-    license="GNU LGPL version 3",
-    packages=["fenics_ice"],
-    python_requires=">=3.8")
+        name="fenics_ice",
+        description="A finite element model framework "
+                    "that quantifies the initialization uncertainty for "
+                    "time-dependent ice sheet models.",
+        url="https://github.com/EdiGlacUQ/fenics_ice.git",
+        license="GNU LGPL version 3",
+        packages=["fenics_ice"],
+        python_requires=">=3.8"
+    )
