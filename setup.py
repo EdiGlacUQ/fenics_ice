1345b2a8504030e6e435920af479a6a9ca2156e7
bearecinos <beatriz.recinos@ed.ac.uk>
2022-07-12 13:09:53 +0100

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import setuptools
+if __name__ == "__main__":
+    setuptools.setup(
+    name="fenics_ice",
+    description="A finite element model framework "
+                "that quantifies the initialization uncertainty for "
+                "time-dependent ice sheet models.",
+    url="https://github.com/EdiGlacUQ/fenics_ice.git",
+    license="GNU LGPL version 3",
+    python_requires=">=3.8")
